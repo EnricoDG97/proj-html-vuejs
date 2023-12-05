93d5f49f7b686a5ddab7972cb364532d6eb42d8d
EnricoDG97 <enrico.delgaudio97@gmail.com>
2023-12-05 18:08:59 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script >
+  import { store } from "./store.js"
+  export default {
+    data() {
+      return {
+        store,
+      }
+    },
+  }
+</script>
+
+<template>
+  <p> test </p>
+</template>
+
+<style lang="scss">
+@use "./style/general.scss";
+@import "@fortawesome/fontawesome-free/css/all.css";
+
+</style>
